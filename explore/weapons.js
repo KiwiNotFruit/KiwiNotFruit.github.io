b599b6b680c1e501b9ecd509f1cf980c0c985fa1
KiwiNotFruit <159232261+KiwiNotFruit@users.noreply.github.com>
2026-09-09 23:43:02 +0200

    Rename 'Recoil Avg' to 'Recoil Rating'

--- explore/weapons.js
+++ explore/weapons.js
@@ -445,7 +445,7 @@
       fields: [
          ['Reload Speed', 'reloadSpeed'],
          ['Fire Rate', 'fireRate'],
-         ['Recoil Avg', 'recoilAvg'],
+         ['Recoil Rating', 'recoilRating'],
          ['Recoil Score', 'recoilScore'],
          ['Accuracy Rating', 'accuracyRating'],
          ['Accuracy Score', 'accuracyScore'],
